838f934e8213bf318547318a3a9003b931e1225c
chauquangabc <chauquangabc@gmail.com>
2025-04-25 21:58:09 +0700

    Order

--- app/src/main/java/com/uilover/project1992/Activity/ProfileActivity.java
+++ app/src/main/java/com/uilover/project1992/Activity/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
@@ -28,6 +29,8 @@ public class ProfileActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private FirebaseUser currentUser;
 
+    private LinearLayout myOrderLayout ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,12 +41,16 @@ public class ProfileActivity extends AppCompatActivity {
         tvUsername = findViewById(R.id.tv_username);
         tvUserEmail = findViewById(R.id.tv_user_email); // ID của TextView
         backBtn = findViewById(R.id.backBtn); // Ánh xạ nút back
-
+        myOrderLayout = findViewById(R.id.openOrder);
         // Khởi tạo Firebase
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
         currentUser = mAuth.getCurrentUser();
 
+        myOrderLayout.setOnClickListener(v -> {
+            Intent intent = new Intent(ProfileActivity.this, MyOrderActivity.class);
+            startActivity(intent);
+        });
 
         ImageButton btnEditProfile = findViewById(R.id.btn_edit_profile);
 
